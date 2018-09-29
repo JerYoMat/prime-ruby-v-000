769c7a9b0dfc41f13236afff5916190b754733f7
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-29 01:16:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,15 @@
 # Add  code here!
-def prime(i)
+def prime(number)
+  counter = 1
+  divisible_by = []
+  while counter < number
+    divisible_by << number / counter if number % counter == 0 
+    counter += 1 
+  end 
+  if divisible_by.length > 2 
+    true 
+  else 
+    false 
+  end 
 
 end 
